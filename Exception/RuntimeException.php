4b204c4a8f069ffdb860bc1bd4eef0dfd65578a7
headzoo <sean@headzoo.io>
2015-05-03 15:56:33 -0400

    Added RuntimeException

---
+++ Exception/RuntimeException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Headzoo\Bundle\PolymerBundle\Exception;
+
+/**
+ * Exception thrown if an error which can only be found on runtime occurs.
+ */
+class RuntimeException
+    extends PolymerException {}
