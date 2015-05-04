091b36c0f158515ef6605b00c5a87bc500ef93e5
headzoo <sean@headzoo.io>
2015-05-04 01:37:01 -0400

    Fixed test PathResolverTest

--- Tests/Util/PathResolverTest.php
+++ Tests/Util/PathResolverTest.php
@@ -90,13 +90,11 @@ class PathResolverTest
      * 
      * @param string $asset
      * @param bool   $debug
-     * @param bool   $relative_on_debug
      * @param string $expected
      */
-    public function testGetImportUrl($asset, $debug, $relative_on_debug, $expected)
+    public function testGetImportUrl($asset, $debug, $expected)
     {
         $this->configuration->setDebug($debug);
-        $this->configuration->getPaths()->setRelativeOnDebug($relative_on_debug);
         $this->assertEquals(
             $expected,
             $this->fixture->getImportUrl($asset)
@@ -112,65 +110,55 @@ class PathResolverTest
     {
         return [
             [ // 0
-                "@PolymerBundle:hello-world/hello-world.html.twig", false, false,
+                "@PolymerBundle:hello-world/hello-world.html.twig", false,
                 "/bundles/polymer/elements/hello-world/hello-world.html",
             ],
             [ // 1
-                "@PolymerBundle:hello-world.html.twig", false, false,
+                "@PolymerBundle:hello-world.html.twig", false,
                 "/bundles/polymer/elements/hello-world/hello-world.html"
             ],
             [ // 2
-                "@PolymerBundle:hello-world", false, false,
+                "@PolymerBundle:hello-world", false,
                 "/bundles/polymer/elements/hello-world/hello-world"
             ],
             [ // 3
-                "paper-elements/paper-elements.html", false, false,
+                "paper-elements/paper-elements.html", false,
                 "/bower_components/paper-elements/paper-elements.html"
             ],
             [ // 4
-                "paper-elements.html", false, false,
+                "paper-elements.html", false,
                 "/bower_components/paper-elements/paper-elements.html"
             ],
             [ // 5
-                "paper-elements", false, false,
+                "paper-elements", false,
                 "/bower_components/paper-elements/paper-elements"
             ],
             [ // 6
-                "/components/paper-elements/paper-elements.html", false, false,
+                "/components/paper-elements/paper-elements.html", false,
                 "/components/paper-elements/paper-elements.html"
             ],
             [ // 7
-                "/components/paper-elements.html", false, false,
+                "/components/paper-elements.html", false,
                 "/components/paper-elements.html"
             ],
             [ // 8
-                "/paper-elements.html", false, false,
+                "/paper-elements.html", false,
                 "/paper-elements.html"
             ],
             [ // 9
-                "/paper-elements", false, false,
+                "/paper-elements", false,
                 "/paper-elements"
             ],
-            /*
             [ // 10
-                "@PolymerBundle:hello-world/hello-world.html.twig", true, true,
-                "/../src/Headzoo/Bundle/PolymerBundle/Resources/public/elements/hello-world/hello-world.html.twig",
+                "@PolymerBundle:hello-world/hello-world.html.twig", true,
+                PolymerConfiguration::ROUTE_IMPORT . "?bundle=PolymerBundle&element=" . urlencode("hello-world/hello-world.html.twig")
             ],
             [ // 11
-                "@PolymerBundle:hello-world/hello-world.html.twig", false, true,
-                "/bundles/polymer/elements/hello-world/hello-world.html",
+                "@PolymerBundle:hello-world.html.twig", true,
+                PolymerConfiguration::ROUTE_IMPORT . "?bundle=PolymerBundle&element=" . urlencode("hello-world/hello-world.html.twig")
             ],
-            */
             [ // 12
-                "@PolymerBundle:hello-world/hello-world.html.twig", true, false,
-                PolymerConfiguration::ROUTE_IMPORT . "?bundle=PolymerBundle&element=" . urlencode("hello-world/hello-world.html.twig")
-            ],
-            [ // 13
-                "@PolymerBundle:hello-world.html.twig", true, false,
-                PolymerConfiguration::ROUTE_IMPORT . "?bundle=PolymerBundle&element=" . urlencode("hello-world/hello-world.html.twig")
-            ],
-            [ // 14
-                "@PolymerBundle:hello-world.html", true, false,
+                "@PolymerBundle:hello-world.html", true,
                 PolymerConfiguration::ROUTE_IMPORT . "?bundle=PolymerBundle&element=" . urlencode("hello-world/hello-world.html")
             ]
         ];
